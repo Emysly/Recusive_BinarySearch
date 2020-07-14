6025f0e14fd25c30add938820a628ca93eaf1058
emysly <emysly12@gmail.com>
2020-07-15 00:26:13 +0100

    binary search implementation using recursive method

---
+++ src/com/company/Recursive_BinarySearch.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Recursive_BinarySearch {
+
+        public static void binarySearch(int arr[], int key){
+            int first = 0;
+            int last = arr.length - 1;
+            int mid = (first + last)/2;
+            while( first <= last ){
+                if ( arr[mid] < key ){
+                    first = mid + 1;
+                }else if ( arr[mid] == key ){
+                    System.out.println("found the number at index " + mid);
+                    break;
+                }else{
+                    last = mid - 1;
+                }
+                mid = (first + last)/2;
+            }
+            if ( first > last ){
+                System.out.println(-1);
+            }
+        }
+        public static void main(String args[]){
+            binarySearch(new int[]{10,20,30,40,50}, 30);
+        }
+
+}
